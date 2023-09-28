19332348f455b1d25dc238bcfb488ceeaf77caf1
Lee Hyohwak <crewe1746@naver.com>
2023-09-28 22:55:30 +0900

    Update TIL: concurrency - sleeping barber

--- go/concurrency/sleeping-barber/barber.go
+++ go/concurrency/sleeping-barber/barber.go
@@ -17,25 +17,23 @@ const (
 )
 
 type Barber struct {
-	Name            string
-	CuttingDuration time.Duration
-	State           BarberState
-	doneChan        chan bool
-	customerChan    <-chan *Customer
+	Name              string        // 이발사의 이름
+	CuttingDuration   time.Duration // 이발사가 머리를 깍는데 걸리는 시간
+	State             BarberState   // 이발사의 상태
+	readyToGoHomeChan chan bool     // 이발사가 퇴근하기 위해 준비하는 채널
+	doneChan          chan bool     // 이발사가 퇴근할 때까지 기다리는 채널
 
-	wg *sync.WaitGroup
-	mu sync.Mutex
+	mu sync.Mutex // 이발사의 상태를 변경할 때 사용하는 뮤텍스
 }
 
-func NewBarber(name string, cuttingDuration time.Duration, wg *sync.WaitGroup) *Barber {
+func NewBarber(name string, cuttingDuration time.Duration) *Barber {
 	return &Barber{
-		Name:            name,
-		CuttingDuration: cuttingDuration,
-		State:           Checking,
-		doneChan:        make(chan bool, 1),
-		customerChan:    nil,
-		wg:              wg,
-		mu:              sync.Mutex{},
+		Name:              name,
+		CuttingDuration:   cuttingDuration,
+		State:             Checking,
+		readyToGoHomeChan: make(chan bool, 1),
+		doneChan:          make(chan bool, 1),
+		mu:                sync.Mutex{},
 	}
 }
 
@@ -45,54 +43,62 @@ func (b *Barber) GoToWork(shop *BarberShop) {
 
 	color.Magenta("%s(은)는 출근합니다.\n", b.Name)
 
-	if err := shop.AddBarber(b); err != nil {
+	customers, err := shop.AddBarber(b) // 바버샵에 이발사를 추가합니다.
+	if err != nil {
 		if errors.Is(err, ErrBarberShopClosed) {
 			color.Red("%s(은)는 출근하지 못했습니다. 바버샵이 문을 닫았습니다.\n", b.Name)
 		}
 		return
 	}
 
-	b.wg.Add(1)
+	color.Magenta("%s(은)는 바버샵에서 일을 시작합니다.\n", b.Name)
 
-	go func() {
-		defer func() {
-			close(b.doneChan)
-		}()
-
-		for {
-			select {
-			case <-b.doneChan:
-				b.doneForToday()
-				return
-			case customer, ok := <-b.customerChan:
-				if !ok || customer == nil {
-					b.doneForToday()
-					return
-				}
-
-				b.mu.Lock()
-				if b.State == Sleeping {
-					color.Green("%s(은)는 %s(을)를 깨웁니다.\n", customer, b.Name)
-					b.State = Checking
-				}
-				b.mu.Unlock()
-
-				b.CutHair(customer)
-			default:
-				b.mu.Lock()
-				if b.State == Checking {
-					color.Magenta("%s(은)는 할 일이 없어 잠을 잡니다.\n", b.Name)
-					b.State = Sleeping
-				}
-				b.mu.Unlock()
-			}
-		}
-	}()
+	go b.acceptCustomers(customers) // 바버샵에 있는 고객들을 받아서 일을 합니다.
 }
 
-func (b *Barber) CutHair(customer *Customer) {
+func (b *Barber) acceptCustomers(customers <-chan *Customer) {
+	for {
+		select {
+		case <-b.readyToGoHomeChan: // 퇴근을 준비합니다.
+			defer func() {
+				close(b.doneChan)
+				close(b.readyToGoHomeChan)
+			}()
+			color.Magenta("%s(은)는 퇴근을 준비합니다.\n", b.Name)
+			time.Sleep(time.Millisecond * 3000)
+			color.Magenta("%s(은)는 오늘 하루 일을 마치고 집으로 돌아갑니다.\n", b.Name)
+			return
+		case customer, ok := <-customers: // 바버샵에 있는 고객들을 받습니다.
+			// 바버샵에 있는 고객들을 받았는데, 바버샵이 문을 닫았거나 고객이 없으면 다음 고객을 받습니다.
+			if !ok || customer == nil {
+				continue
+			}
+
+			b.mu.Lock()
+			// 고객을 받았는데, 이발사가 자고 있으면 고객이 이발사를 깨우고, 이발사의 상태를 체크 중으로 변경합니다.
+			if b.State == Sleeping {
+				color.Green("%s(은)는 %s(을)를 깨웁니다.\n", customer, b.Name)
+				b.State = Checking
+			}
+			b.mu.Unlock()
+
+			b.cutHair(customer) // 이발사가 고객의 머리를 깍습니다.
+		default:
+			b.mu.Lock()
+			// 대기 중인 고객이 없으면 이발사가 잠을 잡니다.
+			if b.State == Checking {
+				color.Magenta("%s(은)는 할 일이 없어 잠을 잡니다.\n", b.Name)
+				b.State = Sleeping
+			}
+			b.mu.Unlock()
+		}
+	}
+}
+
+func (b *Barber) cutHair(customer *Customer) {
 	color.Magenta("%s(은)는 %s의 머리를 깍습니다.\n", b.Name, customer)
 
+	// 머리를 깍습니다.
 	b.mu.Lock()
 	b.State = Cutting
 	b.mu.Unlock()
@@ -101,12 +107,18 @@ func (b *Barber) CutHair(customer *Customer) {
 
 	color.Magenta("%s(은)는 %s의 머리를 다 깍았습니다.\n", b.Name, customer)
 
+	go customer.LeaveBarberShop() // 고객이 머리를 다 깍았으니 집으로 돌아갑니다.
+
+	// 머리를 다 깍았으면 다음 고객을 받습니다.
 	b.mu.Lock()
 	b.State = Checking
 	b.mu.Unlock()
 }
 
-func (b *Barber) doneForToday() {
-	defer b.wg.Done()
-	color.Magenta("%s(은)는 오늘 하루 일을 마치고 집으로 돌아갑니다.\n", b.Name)
+func (b *Barber) GetReadyToGoHome() {
+	b.readyToGoHomeChan <- true // 이발사가 퇴근할 준비가 되었음을 알립니다.
+}
+
+func (b *Barber) Done() <-chan bool {
+	return b.doneChan // 이발사가 퇴근할 때까지 기다립니다.
 }
--- go/concurrency/sleeping-barber/main.go
+++ go/concurrency/sleeping-barber/main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 
 	"github.com/fatih/color"
@@ -14,51 +13,49 @@ func main() {
 	fmt.Println("잠자는 이발사 문제")
 	fmt.Println("==================================================")
 
-	shop := NewBarberShop(10, time.Duration(time.Second)*10)
+	shop := NewBarberShop(10, time.Duration(time.Second)*10) // 10명의 고객을 수용할 수 있고 10초 동안 영업하는 바버샵을 만듭니다.
 
-	wg := sync.WaitGroup{}
+	shop.OpenShop() // 바버샵을 오픈합니다.
 
-	shop.OpenShop()
+	// 바버샵에 이발사들을 추가합니다.
+	NewBarber("철수", 1000*time.Millisecond).GoToWork(shop)
+	NewBarber("영희", 2000*time.Millisecond).GoToWork(shop)
+	NewBarber("영수", 3000*time.Millisecond).GoToWork(shop)
+	NewBarber("민수", 2000*time.Millisecond).GoToWork(shop)
+	NewBarber("민희", 3000*time.Millisecond).GoToWork(shop)
+	NewBarber("국봉", 1000*time.Millisecond).GoToWork(shop)
 
-	NewBarber("철수", 1000*time.Millisecond, &wg).GoToWork(shop)
-	NewBarber("영희", 2000*time.Millisecond, &wg).GoToWork(shop)
-	NewBarber("영수", 3000*time.Millisecond, &wg).GoToWork(shop)
-	NewBarber("민수", 2000*time.Millisecond, &wg).GoToWork(shop)
-	NewBarber("민희", 3000*time.Millisecond, &wg).GoToWork(shop)
-	NewBarber("국봉", 1000*time.Millisecond, &wg).GoToWork(shop)
+	go randomCustomers(shop) // 랜덤한 시간 간격으로 고객들이 바버샵에 들어갑니다.
 
-	go func() {
-		customerId := 1
+	shop.WaitTilAllDone() // 바버샵이 문을 닫고 모든 이발사들이 퇴근할 때까지 기다립니다.
 
-		for {
-			randMillisecond := rand.Int() % 200
-
-			<-time.After(time.Duration(randMillisecond) * time.Millisecond)
-
-			c := NewCustomer(fmt.Sprintf("고객%d", customerId))
-
-			color.Green("%s(이)가 바버샵에 들어갑니다.\n", c)
-
-			err := c.EnterBarberShop(shop)
-			if err != nil {
-				if errors.Is(err, ErrBarberShopClosed) {
-					color.Red("%s(이)가 바버샵에 들어가지 못했습니다. 바버샵이 문을 닫았습니다.\n", c)
-					return
-				} else if errors.Is(err, ErrorCustomerFull) {
-					color.Red("바버샵이 꽉 찼습니다. %s(은)는 집으로 돌아갑니다.\n", c)
-				} else {
-					color.Red("알 수 없는 오류가 발생했습니다. %s(은)는 집으로 돌아갑니다.\n", c)
-				}
-			}
-
-			color.Green("%s(이)가 머리를 자르고 집으로 돌아갑니다.\n", c)
-
-			customerId++
-		}
-	}()
-
-	wg.Wait()
-
-	color.Blue("공지: 모든 이발사가 퇴근했습니다. 바버샵이 문을 닫습니다. 다음에 또 오세요!\n")
 	fmt.Println("==================================================")
+}
+
+func randomCustomers(shop *BarberShop) {
+	customerId := 1
+
+	for {
+		randMillisecond := rand.Int() % 300
+
+		<-time.After(time.Duration(randMillisecond) * time.Millisecond) // 랜덤한 시간 동안 기다립니다.
+
+		c := NewCustomer(fmt.Sprintf("고객%d", customerId)) // 새로운 고객을 만듭니다.
+
+		color.Green("%s(이)가 바버샵에 들어갑니다.\n", c)
+
+		err := c.EnterBarberShop(shop) // 고객이 바버샵에 들어갑니다.
+		if err != nil {
+			if errors.Is(err, ErrBarberShopClosed) {
+				color.Red("%s(이)가 바버샵에 들어가지 못했습니다. 바버샵이 문을 닫았습니다.\n", c)
+				return
+			} else if errors.Is(err, ErrorCustomerFull) {
+				color.Red("바버샵이 꽉 찼습니다. %s(은)는 집으로 돌아갑니다.\n", c)
+			} else {
+				color.Red("알 수 없는 오류가 발생했습니다. %s(은)는 집으로 돌아갑니다.\n", c)
+			}
+		}
+
+		customerId++ // 고객 ID를 증가시킵니다.
+	}
 }
