f085e277a1736e495f89d915c5bef41ea4bf7ee3
Lee Hyohwak <crewe1746@naver.com>
2023-10-17 15:44:32 +0900

    Update TIL: icp-rust implement publisher

--- icp-rust/pubsub/src/publisher/src/lib.rs
+++ icp-rust/pubsub/src/publisher/src/lib.rs
@@ -1,4 +1,42 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Principal};
+use ic_cdk::{caller, notify, update};
+use serde::Deserialize;
+use std::{cell::RefCell, collections::BTreeMap};
+
+type SubscriberStore = BTreeMap<Principal, Subscriber>;
+
+thread_local! {
+    static SUBSCRIBERS: RefCell<SubscriberStore> = RefCell::default();
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Counter {
+    topic: String,
+    value: u64,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Subscriber {
+    topic: String,
+}
+
+#[update]
+fn subscribe(subscriber: Subscriber) {
+    let subscriber_principal_id = caller();
+    SUBSCRIBERS.with(|store| {
+        store
+            .borrow_mut()
+            .insert(subscriber_principal_id, subscriber);
+    });
+}
+
+#[update]
+async fn publish(counter: Counter) {
+    SUBSCRIBERS.with(|store| {
+        for (k, v) in store.borrow().iter() {
+            if v.topic == counter.topic {
+                let _call_result: Result<(), _> = notify(*k, "update_count", (&counter,));
+            }
+        }
+    })
 }
